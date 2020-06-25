46b34a7943e42f1f59fab9da08c853888a8b907e
atanda rasheed <atandarash@gmail.com>
2020-06-26 00:07:21 +0100

    refactor: update drone commands and mix aliases

--- mix.exs
+++ mix.exs
@@ -69,8 +69,9 @@ defmodule ReviewApp.MixProject do
   defp aliases do
     [
       setup: ["deps.get", "ecto.setup", "cmd npm install --prefix assets"],
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.setup": ["ecto.create", "ecto.migrate", "db.seed"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
+      "db.seed": ["run priv/repo/seeds.exs"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
     ]
   end
