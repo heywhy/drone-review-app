5287836635ea53bb6d896fcaa629e15836808627
atanda rasheed <atandarash@gmail.com>
2020-07-05 17:09:44 +0100

    chore: move simple event ui interaction to use alpine

--- lib/api_web/components/navigation/app_bar.ex
+++ lib/api_web/components/navigation/app_bar.ex
@@ -2,17 +2,4 @@ defmodule ReviewAppWeb.Components.Navigation.AppBar do
   use ReviewAppWeb, :live_component
 
   alias ReviewAppWeb.Components.Icons
-
-  def mount(socket) do
-    {:ok, assign(socket, :isAccountMenuOpened, false)}
-  end
-
-  def handle_event("account-menu", _args, socket) do
-    state = !socket.assigns.isAccountMenuOpened
-    {:noreply, assign(socket, :isAccountMenuOpened, state)}
-  end
-
-  def handle_event("close:account-menu", _args, socket) do
-    {:noreply, assign(socket, :isAccountMenuOpened, false)}
-  end
 end
