f2713b35206fb759dc265900abb3949bc8642958
atanda rasheed <atandarash@gmail.com>
2020-07-24 02:09:22 +0100

    chore: include icons in as component

---
+++ assets/src/components/icons/index.js
@@ -0,0 +1,7 @@
+import CogIcon from './Cog.vue'
+import BellIcon from './Bell.vue'
+import GithubIcon from './Github.vue'
+import LogoutIcon from './Logout.vue'
+import ArrowLongDownIcon from './ArrowLongDown.vue'
+
+export { CogIcon, BellIcon, GithubIcon, LogoutIcon, ArrowLongDownIcon }
